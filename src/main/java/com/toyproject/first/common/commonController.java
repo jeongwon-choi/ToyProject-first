a3aa6c315a281fc584d9546764c8ecd83586aa45
OGong <ogong@OGongui-MacBookAir.local>
2022-09-24 16:06:15 +0900

    pull test

--- src/main/java/com/toyproject/first/common/commonController.java
+++ src/main/java/com/toyproject/first/common/commonController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class commonController {
     @GetMapping(value = "")
     public String home(){
-        return "Hellow VS code";
+        return "Hellow VS code!!!!!!";
     }
 }
