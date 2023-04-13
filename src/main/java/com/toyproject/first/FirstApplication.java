2cb332e43d12640920683cdfa9ecd7899c4d9f8c
Ogong <chol0501@nate.com>
2023-04-13 18:55:20 +0900

    mariaDB connection & JPA 셋팅 작업중

--- src/main/java/com/toyproject/first/FirstApplication.java
+++ src/main/java/com/toyproject/first/FirstApplication.java
@@ -2,8 +2,10 @@ package com.toyproject.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // BaseEntity클래스의 AuditingEntityListener를 활성화 시키기 위해 추가한다
 public class FirstApplication {
 
 	public static void main(String[] args) {
