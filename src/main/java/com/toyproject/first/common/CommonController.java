0a11f736bcf9e014cfe47b446c8d6fafeb26acdb
Seung Ho Lee <shlee@enliple.com>
2023-04-13 17:43:30 +0900

    DBConnection & JPA 작업중

--- src/main/java/com/toyproject/first/common/CommonController.java
+++ src/main/java/com/toyproject/first/common/CommonController.java
@@ -1,5 +1,7 @@
 package com.toyproject.first.common;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
+@RequiredArgsConstructor
 @RequestMapping(value = "/")
-public class commonController {
+public class CommonController {
+    private final CommonService commonService;
+
     @GetMapping(value = "")
     public String home(Model model){
         model.addAttribute("hello","Hello Thymeleaf Test");
+        commonService.mariadbConnectionTest();
         return "index";
     }
 }
