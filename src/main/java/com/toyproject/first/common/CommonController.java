0433dc0dda9bd9b6e392727a6ce29a4f2f2bd45d
Ogong <chol0501@nate.com>
2023-04-13 19:17:12 +0900

    mariaDB Connection & JPA setting 작업중

--- src/main/java/com/toyproject/first/common/CommonController.java
+++ src/main/java/com/toyproject/first/common/CommonController.java
@@ -1,5 +1,7 @@
 package com.toyproject.first.common;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
+@RequiredArgsConstructor
 @RequestMapping(value = "/")
-public class commonController {
+public class CommonController {
     @GetMapping(value = "")
     public String home(Model model){
         model.addAttribute("hello","Hello Thymeleaf Test");
