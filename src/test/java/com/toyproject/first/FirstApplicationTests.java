25596a53db3cbb87c758124b1ab3915b324575aa
Ogong <chol0501@nate.com>
2023-04-17 16:38:58 +0900

    @SpringBootTest 주석 처리

--- src/test/java/com/toyproject/first/FirstApplicationTests.java
+++ src/test/java/com/toyproject/first/FirstApplicationTests.java
@@ -3,9 +3,8 @@ package com.toyproject.first;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FirstApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
