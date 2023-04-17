d0e580e40addeab7d954c7caff3ee0734186637b
Ogong <chol0501@nate.com>
2023-04-17 16:02:49 +0900

    환경변수 셋팅완료

--- src/main/java/com/toyproject/first/common/CommonController.java
+++ src/main/java/com/toyproject/first/common/CommonController.java
@@ -1,12 +1,10 @@
 package com.toyproject.first.common;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 //@RestController
